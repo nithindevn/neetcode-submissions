89493e123a263c52e46e457c1cc2bf9af650d8a4
nithindevn <nithindevn@users.noreply.github.com>
2026-07-21 11:39:25 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+
+        guard s.count == t.count else {
+            return false
+        }
+
+        var sDict: [Character: Int] = [:]
+        var tDict: [Character: Int] = [:]
+
+        for sChar in s {
+            sDict[sChar, default: 0] += 1
+        }
+
+        for tChar in t {
+            if let count = tDict[tChar] {
+                tDict[tChar] = count+1
+            } else {
+                tDict[tChar] = 1
+            }
+        }
+
+        for (key, value) in sDict {
+            if tDict[key] != value {
+                return false
+            }
+        }
+
+        return true
+    }
+}
